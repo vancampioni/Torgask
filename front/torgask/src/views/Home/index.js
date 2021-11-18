5d90ba0303b6159f499473538a410e9d13f387a0
vancampioni <vanessacrtsilva@gmail.com>
2021-11-17 22:05:39 -0300

    add footer

--- front/torgask/src/views/Home/index.js
+++ front/torgask/src/views/Home/index.js
@@ -3,12 +3,14 @@ import * as S from './styled';
 
 // COMPONENTES
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 function Home() {
     return (
       <S.Container>
 
         <Header />
+        <Footer />
       </S.Container>
     );
   }
